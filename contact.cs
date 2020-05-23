4115769ee4e43ad3cbff2e39c919074a5f5874a3
Neliyenn <gocha1103@gmail.com>
2020-05-23 20:24:56 +0200

    Create contact.cs

---
+++ contact.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone_Emulator
+{
+    public class ContactClass
+    {
+        string contactName;
+        string contactNumber;
+
+        public string number
+        {
+            get
+            {
+                string correctNumber = "";
