023528ff544491003c9bd1b9a62a3ee21649d48c
Neliyenn <gocha1103@gmail.com>
2020-05-23 20:47:07 +0200

    Update contact.cs

--- contact.cs
+++ contact.cs
@@ -26,3 +26,48 @@ namespace Phone_Emulator
             get => contactName;
             set => contactName = value;
         }
+
+        public string FindingNumber() => contactNumber;
+
+        public string NumberConvertWithSpaces(string convert)
+        {
+            string correctNumber = "";
+
+            string[] buffer = convert.Split(' ');
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                correctNumber += buffer[i];
+            }
+
+            return correctNumber;
+        }
+
+        public string NumberConvertWithoutSpaces(string convert)
+        {
+            string correctNumber = "";
+
+            if(convert.Length == 8)
+            {
+                if (PhoneDatabase.prefixes.Contains(int.Parse(convert[0] + "" + convert[1])))
+                {
+                    for (int first = 0; first < 2; first++)
+                    {
+                        correctNumber += convert[first];
+                    }
+
+                    correctNumber += " ";
+
+                    for (int sec = 2; sec < 4; sec++)
+                    {
+                        correctNumber += convert[sec];
+                    }
+
+                    correctNumber += " ";
+
+                    for (int third = 4; third < 8; third++)
+                    {
+                        correctNumber += convert[third];
+                    }
+                }
+            }
