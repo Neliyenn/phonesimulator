bf67673f45f29fb0663c233af083226f7c63e21d
Neliyenn <gocha1103@gmail.com>
2020-05-24 23:55:53 +0200

    Update menu.cs

--- menu.cs
+++ menu.cs
@@ -28,3 +28,90 @@ namespace Phone_Emulator
             ">>> CONTACTS LIST <<<",
             "{Press any key to return}"
         };
+
+        public static List<string> addContactMenu = new List<string>
+        {
+            ">>> ADDING CONTACT <<<",
+            "{Type exit to return}",
+            "Type name or number to add:"
+        };
+
+        public static List<string> removeContactMenu = new List<string>
+        {
+            ">>> REMOVING CONTACT <<<",
+            "{Type exit to return}",
+            "Type name or number to remove:"
+        };
+
+        public static void ProceedCommand(int commandType, string command)
+        {
+            if (command == "exit")
+                return;
+
+            switch(commandType)
+            {
+                case 1:
+                    {
+                        break;
+                    }
+                case 2:
+                    {
+                        foreach (ContactClass contact in PhoneDatabase.contacts)
+                        {
+                            Console.WriteLine("{0}: {1}", contact.name, contact.number);
+                        }
+
+                        Console.ReadKey(true);
+                        PhoneCore.ForceReturn();
+
+                        break;
+                    }
+                case 3:
+                    {
+                        if(long.TryParse(command, out long buffer))
+                        {
+                            ContactClass contact = new ContactClass();
+                            contact.number = command;
+
+                            Console.Write("Phone name: ");
+                            contact.name = Console.ReadLine();
+
+                            AddContact(contact);
+                        }
+                        else
+                        {
+                            ContactClass contact = new ContactClass();
+                            contact.name = command;
+
+                            Console.Write("Phone number: ");
+                            string number = Console.ReadLine();
+
+                            if(number.Contains(' '))
+                            {
+                                number = contact.NumberConvertWithSpaces(number);
+                            }
+
+                            if(long.TryParse(number, out long secBuffer))
+                            {
+                                contact.number = number;
+
+                                AddContact(contact);
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Wrong Number!!!!");
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.WriteLine("{Press any key to return}");
+                                Console.ReadKey(true);
+                                PhoneCore.ForceReturn();
+                            }
+                        }
+                        break;
+                    }
+                case 4:
+                    {
+                        break;
+                    }
+            }
+        }
