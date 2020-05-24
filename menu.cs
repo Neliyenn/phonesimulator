a3562246047d6a1dcf1c9d4d877827770eb42e50
Neliyenn <gocha1103@gmail.com>
2020-05-24 22:50:47 +0200

    Update menu.cs

--- menu.cs
+++ menu.cs
@@ -2,3 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+
+namespace Phone_Emulator
+{
+    public static class MenuManager
+    {
+        public static List<string> startMenu = new List<string>
+        {
+            "1. Find a number/contact",
+            "2. Show all contacts",
+            "3. Add Contact",
+            "4. Remove Contact",
+            "5. Turn Off"
+        };
+
+        public static List<string> findContactMenu = new List<string>
+        {
+            ">>> FINDING CONTACT <<<",
+            "{Type exit to return}",
+            "Type name or number to find:"
+        };
