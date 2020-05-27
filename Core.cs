39df0b2084a447e3d86fa1a310aae5cd0cce6a67
Neliyenn <gocha1103@gmail.com>
2020-05-27 20:53:45 +0200

    Update Core.cs

--- Core.cs
+++ Core.cs
@@ -11,4 +11,22 @@ namespace Phone_Emulator
         public static void Start()
         {
             PhoneScreen.Initialize(MenuManager.startMenu);
+        while(true)
+            {
+                ConsoleKey response = new ConsoleKey();
+                string message = "";
+
+                if (typingMenu == 0)
+                    response = Console.ReadKey(true).Key;
+                else if(typingMenu == 2)
+                    MenuManager.ProceedCommand(2, "");
+                else
+                {
+                    message = Console.ReadLine();
+
+                    MenuManager.ProceedCommand(typingMenu, message);
+                }
+            }
         }
+    }
+}
